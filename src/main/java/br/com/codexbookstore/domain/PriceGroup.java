13661d7dea5e3c9a31af91d63a24860262bf28a3
Everton Silva <thonns@gmail.com>
2017-09-24 17:15:43 -0300

    chore: create PriceGroup class

---
+++ src/main/java/br/com/codexbookstore/domain/PriceGroup.java
@@ -0,0 +1,16 @@
+package br.com.codexbookstore.domain;
+
+/**
+ * Created by everton on 24/09/17.
+ */
+public class PriceGroup {
+    private double markup;
+
+    public double getMarkup() {
+        return markup;
+    }
+
+    public void setMarkup(double markup) {
+        this.markup = markup;
+    }
+}
