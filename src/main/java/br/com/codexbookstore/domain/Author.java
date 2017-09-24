f7521acd834d9bc5aba506f319e9c3316e7cd03f
Everton Silva <thonns@gmail.com>
2017-09-24 17:19:43 -0300

    chore: create Author class

---
+++ src/main/java/br/com/codexbookstore/domain/Author.java
@@ -0,0 +1,16 @@
+package br.com.codexbookstore.domain;
+
+/**
+ * Created by everton on 24/09/17.
+ */
+public class Author {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
