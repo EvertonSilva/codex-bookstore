569420574085494195ef795a48bad5df821e31b6
Everton Silva <thonns@gmail.com>
2017-09-24 17:11:40 -0300

    chore: create Entity class to get a base class for all domain entities

---
+++ src/main/java/br/com/codexbookstore/domain/Entity.java
@@ -0,0 +1,25 @@
+package br.com.codexbookstore.domain;
+
+/**
+ * Created by everton on 24/09/17.
+ */
+public class Entity {
+    private Long id;
+    private boolean enabled;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
