53f87f35a7aa23604b45782f42adf1607ca64ab2
Everton Silva <thonns@gmail.com>
2017-09-24 17:17:48 -0300

    chore: create Dimensions class

---
+++ src/main/java/br/com/codexbookstore/domain/Dimensions.java
@@ -0,0 +1,43 @@
+package br.com.codexbookstore.domain;
+
+/**
+ * Created by everton on 24/09/17.
+ */
+public class Dimensions {
+    private double depth;
+    private double height;
+    private double weight;
+    private double width;
+
+    public double getDepth() {
+        return depth;
+    }
+
+    public void setDepth(double depth) {
+        this.depth = depth;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+}
