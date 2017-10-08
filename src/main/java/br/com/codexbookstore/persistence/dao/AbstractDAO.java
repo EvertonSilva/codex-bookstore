ff6c8903966b5aec4ef3363ca91b6f6e4c0d83d1
Everton Silva <thonns@gmail.com>
2017-10-08 01:30:47 -0300

    feat: use AbstractDAO class to incapsulate common functions related to connection with the database

--- src/main/java/br/com/codexbookstore/persistence/dao/AbstractDAO.java
+++ src/main/java/br/com/codexbookstore/persistence/dao/AbstractDAO.java
@@ -1,4 +1,23 @@
 package br.com.codexbookstore.persistence.dao;
 
+import br.com.codexbookstore.persistence.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public abstract class AbstractDAO implements IDAO {
+
+    protected Connection conn;
+
+    public void openConnection() {
+        conn = new ConnectionFactory().getConnection();
+    }
+
+    public void closeConnection() {
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
