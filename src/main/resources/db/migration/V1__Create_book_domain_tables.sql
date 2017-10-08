508102ca4d6813809082b9c0aaef1fcc98d3c6f0
Everton Silva <thonns@gmail.com>
2017-10-08 01:09:36 -0300

    chore: update flyway script to create tables for persist a Book

---
+++ src/main/resources/db/migration/V1__Create_book_domain_tables.sql
@@ -0,0 +1,72 @@
+CREATE TABLE `books` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `enabled` BOOLEAN NOT NULL,
+  `title` VARCHAR(255) NOT NULL,
+  `edition` VARCHAR(255) NOT NULL,
+  `synopsis` VARCHAR(255) NOT NULL,
+  `isbn` VARCHAR(255) NOT NULL,
+  `barcode` VARCHAR(255) NOT NULL,
+  `publishYear` VARCHAR(255) NOT NULL,
+  `numberOfPages` VARCHAR(255) NOT NULL,
+  `height` INT NOT NULL,
+  `width` INT NOT NULL,
+  `depth` INT NOT NULL,
+  `weight` INT NOT NULL,
+  `author_id` INT NOT NULL,
+  `sales_paremeters_id` INT NOT NULL,
+  `price_group_id` INT NOT NULL,
+  `publisher_id` INT NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `authors` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `publishers` (
+  `id` BINARY NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `categories` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `description` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `sales_parametrization` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `min_sale_limit` INT NOT NULL,
+  `periodicity` INT NOT NULL,
+  `periodicity_unit` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `price_group` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `books_categoiries` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `book_id` INT NOT NULL,
+  `categoy_id` INT NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+ALTER TABLE `books` ADD CONSTRAINT `books_fk0` FOREIGN KEY (`author_id`) REFERENCES `authors`(`id`);
+
+ALTER TABLE `books` ADD CONSTRAINT `books_fk1` FOREIGN KEY (`sales_paremeters_id`) REFERENCES `sales_parametrization`(`id`);
+
+ALTER TABLE `books` ADD CONSTRAINT `books_fk2` FOREIGN KEY (`price_group_id`) REFERENCES `price_group`(`id`);
+
+ALTER TABLE `books` ADD CONSTRAINT `books_fk3` FOREIGN KEY (`publisher_id`) REFERENCES `publishers`(`id`);
+
+ALTER TABLE `books_categoiries` ADD CONSTRAINT `books_categoiries_fk0` FOREIGN KEY (`book_id`) REFERENCES `books`(`id`);
+
+ALTER TABLE `books_categoiries` ADD CONSTRAINT `books_categoiries_fk1` FOREIGN KEY (`categoy_id`) REFERENCES `categories`(`id`);
+
