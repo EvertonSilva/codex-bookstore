dfda166083fd808f87d2b7c2d0fe62fee7f2a78c
Everton Silva <thonns@gmail.com>
2017-10-12 22:15:28 -0300

    chore: config H2 to run on mixed mode

--- src/main/java/br/com/codexbookstore/persistence/ConnectionFactory.java
+++ src/main/java/br/com/codexbookstore/persistence/ConnectionFactory.java
@@ -5,13 +5,26 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
+    private String dbName = "codex";
+    private String dbPath = "/home/everton/Projetos/les/codex-bookstore/src/main/resources/data/";
+    private String dbUser = "gutenberg";
+    private String dbPass = "letter4thepeople";
+    private String dbMode = "AUTO_SERVER=TRUE";
+    private final StringBuilder DBURL = new StringBuilder("jdbc:h2:file:");
+//    private final String DBURL = "jdbc:h2:file:/home/everton/Projetos/les/codex-bookstore/src/main/resources/data/codex";
 
-    private final String DBURL = "jdbc:h2:file:/home/everton/Projetos/les/codex-bookstore/src/main/resources/data/codex";
+
+    public ConnectionFactory() {
+        DBURL.append(dbPath);
+        DBURL.append(dbName);
+        DBURL.append(";");
+        DBURL.append(dbMode);
+    }
 
     public Connection getConnection() {
         try {
             Class.forName("org.h2.Driver");
-            return DriverManager.getConnection(DBURL, "sa", "");
+            return DriverManager.getConnection(DBURL.toString(), dbUser, dbPass);
         } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
---
+++ src/main/resources/db/migration/V3__Create_db_user_for_app.sql
@@ -0,0 +1,2 @@
+CREATE USER gutenberg PASSWORD 'letter4thepeople';
+GRANT ALL ON SCHEMA PUBLIC TO gutenberg;
