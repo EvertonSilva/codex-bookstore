cd9d6e9b1c441b4b03b18c1df73db2355d8ab338
Everton Silva <thonns@gmail.com>
2017-11-29 00:22:37 -0200

    fix: column name on migration to create stock table

--- src/main/resources/db/migration/V3__Create_table_stocks.sql
+++ src/main/resources/db/migration/V3__Create_table_stocks.sql
@@ -2,7 +2,7 @@ CREATE TABLE `stocks` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `book_id` INT NOT NULL REFERENCES `books`(`id`),
   `quantity` INT NOT NULL,
-  `sale_price` FLOAT NOT NULL,
+  `purchase_price` FLOAT NOT NULL,
   `created_at` TIMESTAMP NOT NULL,
   `updated_at` TIMESTAMP NOT NULL,
   PRIMARY KEY (`id`)
